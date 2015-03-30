bb8b6d871e84f5d3d9bee2fca9dcc42076df7548
Bigdave58 <davidelhunt@gmail.com>
2015-03-31 04:22:51 +1000

    did the fix as requested -sam

--- JavaApplication/src/javaapplication/JavaApplication.java
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -19,6 +19,8 @@ public class JavaApplication {
         // TODO code application logic here
         
         // Hey sam can you fix this line 
+        
+        // all fixed now - sam 
     }
     
 }
